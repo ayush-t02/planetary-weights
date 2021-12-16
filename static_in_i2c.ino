7dec2813e3de567fd19e8c9075a1d5311240a5e1
Riya Modi <87192647+rjmodi3654@users.noreply.github.com>
2021-12-16 17:13:02 +0530

    changes done

--- static_in_i2c.ino
+++ static_in_i2c.ino
@@ -3,58 +3,69 @@
 #include <SoftwareSerial.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-SoftwareSerial knob_input(10, 11);
-double weight = 62.78;
-double knob;
+//SoftwareSerial knob_input(10, 11);
+double weight = 90.78;
+float ans;
+int knob;
 
 void setup()
 {
     lcd.init();
     lcd.backlight();
     Serial.begin(9600);
-    knob_input.begin(9600);
+//    knob_input.begin(9600);
 }
 
 void loop()
 {
-    if (knob_input.available() > 0)
+    while (true)
     {
-        knob = knob_input.read();
+        knob = analogRead(A0);
+        Serial.write(knob);
 
-        if ((knob > 0) && (knob <= 300))
+        if ((knob > 0) && (knob <= 200))
         {
+            ans = weight * 0.38;
             lcd.setCursor(0, 0);
-            lcd.digitalWrite("Sun: ");
+            lcd.print("Mars: ");
             lcd.setCursor(0, 1);
-            lcd.digitalWrite(weight * 27.01);
+            lcd.print(ans, 2);
+            lcd.setCursor(6,0);
+            lcd.print("        ");
         }
-        if ((knob > 300) && (knob <= 600))
+        if ((knob > 200) && (knob <= 400))
         {
+            ans = weight * 1.06;
             lcd.setCursor(0, 0);
-            lcd.digitalWrite("Mercury: ");
+            lcd.print("Saturn: ");
             lcd.setCursor(0, 1);
-            lcd.digitalWrite(weight * 0.38);
+            lcd.print(ans, 2);
         }
-        if ((knob > 600) && (knob <= 900))
+        if ((knob > 400) && (knob <= 600))
         {
+            ans = weight * 0.166;
             lcd.setCursor(0, 0);
-            lcd.digitalWrite("Moon: ");
+            lcd.print("Moon: ");
             lcd.setCursor(0, 1);
-            lcd.digitalWrite(weight * 0.166);
+            lcd.print(ans, 2);
+            lcd.setCursor(6,0);
+            lcd.print("        ");
         }
-        if ((knob > 900) && (knob <= 1200))
+        if ((knob > 600) && (knob <= 800))
         {
+            ans = weight * 0.38;
             lcd.setCursor(0, 0);
-            lcd.digitalWrite("Jupiter: ");
+            lcd.print("Mercury: ");
             lcd.setCursor(0, 1);
-            lcd.digitalWrite(weight * 2.34);
+            lcd.print(ans, 2);
         }
-        if ((knob > 1200) && (knob <= 1500))
+        if ((knob > 800) && (knob <= 1000))
         {
+            ans = weight * 1.19;
             lcd.setCursor(0, 0);
-            lcd.digitalWrite("Neptune: ");
+            lcd.print("Neptune: ");
             lcd.setCursor(0, 1);
-            lcd.digitalWrite(weight * 1.19);
+            lcd.print(ans, 2);
         }
     }
 }
